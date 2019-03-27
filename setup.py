69a3f65ec9820e3d52ba00e0b7e746632cafc569
Jens Vagelpohl <jens@mineralware.com>
2019-03-27 17:21:39 -0500

    - the README is ReST, not Markdown

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-readme = open('README.md').read()
+readme = open('README.rst').read()
 history = open('CHANGES.txt').read()
 long_description = readme + '\n\n' + history
 
